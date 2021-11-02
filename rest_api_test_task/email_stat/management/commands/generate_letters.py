73c74320780d95e810ce3195a33280425a29301c
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2021-11-02 21:53:53 +0200

    fake saved

--- rest_api_test_task/email_stat/management/commands/generate_letters.py
+++ rest_api_test_task/email_stat/management/commands/generate_letters.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand, CommandError
 from factory.faker import faker
 import datetime
 
+from email_stat.models import Letter
 
 class Command(BaseCommand):
     help = 'Generate latters in db'
@@ -11,14 +12,19 @@ class Command(BaseCommand):
         print('How many?')
         n = int(input())
         for i in range(n):
-        # for i in range(5):
             self.generate_latters_history()
 
     def generate_latters_history(self):
         fake = faker.Faker()
         print(
-            fake.email(), 
-            fake.date_time_between(datetime.datetime.now(), datetime.timedelta(days=7)),
-            fake.name()
+            
             )
-
+        letter = Letter(
+            email = fake.email(),
+            date = fake.date_time_between(
+                datetime.datetime.now(), 
+                datetime.timedelta(days=7),
+                ),
+            theme = fake.name()
+        )
+        letter.save()
