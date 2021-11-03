93789ef60e417ec94575dac8a32b1506fd869a6e
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2021-11-03 03:00:08 +0200

    total letters

--- rest_api_test_task/email_stat/views.py
+++ rest_api_test_task/email_stat/views.py
@@ -1,9 +1,24 @@
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
 
 from .serializers import LetterSerializer
 from .models import Letter
 
+from rest_framework.response import Response
+
+
 class LetterViewSet(viewsets.ModelViewSet):
     queryset = Letter.objects.all().order_by('date')
     serializer_class = LetterSerializer
+
+
+@api_view()
+def stat_24(request):
+    total = len(Letter.objects.all())
+    return Response({
+        "total": total,
+        })
+
+
+
 
